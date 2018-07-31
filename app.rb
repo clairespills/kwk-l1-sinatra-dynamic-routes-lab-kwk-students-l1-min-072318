9a066c50622b1c4279970cb276b994cacaf6f555
clairespills <purpleprincess372@gmail.com>
2018-07-31 19:46:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,14 +23,13 @@ get '/:operation/:number1/:number2' do
  @number1 = params[:number1].to_i
  @number2 = params[:number2].to_i
  if @operation == "add" 
-   @output = @number1 + @number2
+  @number1 + @number2
  elsif @operation == "subtract" 
-   @output = @number1 - @number2 
+  @number1 - @number2 
  elsif @operation == "divide" 
-   @output = @number1 / @number2 
+  @number1 / @number2 
  elsif @operation == "multiply" 
-   @output = @number1 * @number2
+  @number1 * @number2
 end
-puts "#{@output}"
 end
 end

a8f5a4ffb0c1e515043efc7d246cc88d5e656ee4
clairespills <purpleprincess372@gmail.com>
2018-07-31 19:39:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,9 +22,9 @@ get '/:operation/:number1/:number2' do
  @operation = params[:operation]
  @number1 = params[:number1].to_i
  @number2 = params[:number2].to_i
- if @operation = "add" 
+ if @operation == "add" 
    output = @number1 + @number2
- elsif params[:operation] = "subtract"
+ elsif 
   params[:number1].to_i - params[:number2].to_i
  elsif params[:operation] = "divide"
   params[:number1].to_i / params[:number2].to_i
